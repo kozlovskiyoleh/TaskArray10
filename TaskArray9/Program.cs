87c9f8079774a6a1e53e3082486053903c1e5b48
Kozlovskiyoleh <oleg.kozlovskiy.35p@gmail.com>
2022-07-21 12:35:18 +0300

    Добавьте файлы проекта.

---
+++ TaskArray9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskArray9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArrayTask 9");
+            var array = new int[10];
+            Console.Write("Count position to shift array: ");
+            int positionShift = int.Parse(Console.ReadLine());
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i + 1;
+            foreach (int i in array)
+                Console.Write(i + " ");
+            for (int i = 0; i < positionShift; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+            Console.WriteLine();
+            foreach (int i in array)
+                Console.Write(i + " ");
+            Console.ReadKey();
+        }
+    }
+}
